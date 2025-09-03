a2d2ef46055829523e4c7a6cbd37a3d99e44cdba
sean-morris <sean.smorris@berkeley.edu>
2025-09-03 14:41:26 -0700

    Initial commit

---
+++ NameMain.java
@@ -0,0 +1,7 @@
+public class NameMain{
+    public static void main(String[] args) {
+        Name n = new Name("Sean","Michael","Morris");
+        System.out.println(n.lastFirst());
+        System.out.println(n.fullName());
+    } 
+}
